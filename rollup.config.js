94564a6b61d9e3ecebfd78ce315305b584864b4a
Brody Smith <62872303+brodysmith1@users.noreply.github.com>
2023-08-11 17:05:47 +1000

    Revert file directory

--- rollup.config.js
+++ rollup.config.js
@@ -35,8 +35,8 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		// file: 'public/build/bundle.js',
-		dir: 'public/build/dylan-ipsum'
+		file: 'public/build/bundle.js',
+		// dir: 'public/build'
 	},
 	plugins: [
 		svelte({
